4147473d257ff7826cfc433609a1d4413bc5ec7b
Zakhar Slinin <slininzahar9@gmail.com>
2026-03-27 16:58:49 +0300

    Update 000001_create_users_table.up.sql

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -2,8 +2,12 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    is_active BOOLEAN NOT NULL,
+		username TEXT NOT NULL, 
+		avatar TEXT, 
+		firstname TEXT NOT NULL, 
+		surname TEXT,
     is_verified BOOLEAN DEFAULT FALSE,
+		is_active BOOLEAN NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
