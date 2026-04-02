2c587289b854990d50d04149a7d1a35da56cfe64
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-04-02 15:45:16 +0300

    feat: create email verifications table

---
+++ db/migrations/000002_create_email_verifications_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE email_verifications (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    token_hash TEXT NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+);
