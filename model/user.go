97c001871373f2d0d1e6283a15853e562de0cbfd
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2026-04-02 15:26:36 +0300

    update: user model | CreateUserRequest, EmailVerificationToken

--- model/user.go
+++ model/user.go
@@ -1,11 +1,14 @@
 package model
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type User struct {
-	ID           string    `json:"id"`
+	ID           uuid.UUID `json:"id"`
 	Email        string    `json:"email"`
-	PasswordHash string    `json:"-"`
+	PasswordHash []byte    `json:"-"`
 	Username     string    `json:"username"`
 	Avatar       string    `json:"avatar"`
 	FirstName    string    `json:"first_name"`
@@ -14,4 +17,21 @@ type User struct {
 	IsActive     bool      `json:"is_active"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type CreateUserRequest struct {
+	Email     string
+	Password  string
+	Username  string
+	Avatar    string
+	FirstName string
+	Surname   string
+}
+
+type EmailVerificationToken struct {
+	ID        uuid.UUID
+	UserID    uuid.UUID
+	Token     string
+	ExpiresAt time.Time
+	CreatedAt time.Time
 }
